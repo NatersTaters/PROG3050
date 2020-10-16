5cd7bd899873ba192d49b3817dc9e46d2de8565d
Nathaniel Saunders <Nsaunders4659@conestogac.on.ca>
2020-10-16 09:20:17 -0400

    Added Validations to Register and Change Account Details pages

--- PROG3050_CVGSClub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PROG3050_CVGSClub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,7 +46,7 @@ namespace PROG3050_CVGSClub.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [Display(Name = "Username")]
             public string Username { get; set; }
 
@@ -56,7 +56,7 @@ namespace PROG3050_CVGSClub.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
