2969f44017797b88f6a6d126ce4a75a8d2cac13c
Leunard Gervalla <Lgervalla8340@conestogac.on.ca>
2020-11-22 12:13:49 -0800

    Removed Delete, Edit Wishlists, UnitTests wip

---
+++ XUnitTestProject/WishListUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROG3050_CVGSClub.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class WishListUnitTest
+    {
+        Random r = new Random();
+        WishLists wishlist;
+        Games game;
+        CvgsClubContext context = new CvgsClubContext();
+
+        private void Initialize()
+        {
+            try { context.Entry(game).State = EntityState.Detached; }
+            catch (Exception) { }
+
+            game = new Games()
+            {
+                GameId = r.Next(),
+                GameName = "GameTest",
+                ListPrice = 60,
+                ContentRating = "M",
+                Genre = "Adventure",
+                AvailablePlatforms = "PC",
+                MaxPlayers = "100"
+            };
+
+            wishlist = new WishLists()
+            {
+                WishId = r.Next(),
+                MemberId = "40",
+                GameId = r.Next(), 
+                Game = game,
+            };
+        }
+
+        [Fact]
+        public void GoodWishlistAddition_AllowTheAddition()
+        {
+            //Arrange
+            Initialize();
+
+            //Act
+            context.Games.Add(game);
+
+            //Assert
+            context.EFValidation();
+        }
+    }
+}
